1d0f4f91ddb2b5e12597bc499c7cd84baa04e233
nhuthuyvinhlong978 <100194410+nhuthuyvinhlong978@users.noreply.github.com>
2022-02-24 16:46:53 +0700

    update fav function

--- src/Screens/Home/Screens/HomeScreens.jsx
+++ src/Screens/Home/Screens/HomeScreens.jsx
@@ -1,7 +1,8 @@
+import React,{useEffect, useState} from "react";
 import education from "../../../Resource/education.json"
 import Navigation from "../../../Components/Navigation"
 import "../../../App.css";
-import { useHistory } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 export default function HomeScreens(){
     <Navigation></Navigation>
     const history = useHistory();
@@ -9,34 +10,81 @@ export default function HomeScreens(){
     const hanldeClick = (slug) => {
         history.push(`/details/${slug}`)
     }
+    const [like, setLike] = useState([])
+    const getStore = () => {
+        const likeStored = localStorage.getItem('like-storage')
+        likeStored === null ? setLike([]) : setLike(JSON.parse(likeStored))
+    }
+    const setStore = (data) => {
+        localStorage.setItem("like-storage",JSON.stringify(data))
+        setLike(data)
+    }
+    
+    useEffect(() => {
+        getStore()
+    }, [])
+    const handleLike = (slug) => {
+        const temp = [...like]
+        const position = temp.indexOf(slug)
+        // if(temp.lastIndexOf === -1){0 
+        //     setLike(like) => [...like, slug])
+        // } dung khi co nhieu slug
+
+        // position === -1?
+        //     temp.push(slug)
+        // :
+        //     temp.splice(position,1)
+        if(position === -1){
+            temp.push(slug)
+        }else{
+            temp.splice(position,1)
+        }
+        setStore(temp)
+    }
     const lists = education?.course?.map((element, index) => {
         return (
-            <div className="col-md-6 col-sm-12 col-lg-4 mt-3">
-                <div className="course-item">
-                <img className='course-image' onClick={() => {hanldeClick(element.slug)}} src={element.image} alt="" />
-                    <div className="course-body"></div>
-                    <h5 className="course-name" onClick={() => {hanldeClick(element.slug)}}>{element.name}</h5>
-                    <p><span class="course-info">Ngày khai giảng: </span>{element.startDate}</p>
-                    <div class="course-info"><span>Trình độ: </span>{element.level}</div>
-                    <div class="course-info"><span>Lịch học: </span>{element.schedule}</div>
-                    <div class="course-info"><span>Thời gian:</span> {element.time}</div>
-                    <div class="course-info"><span>Dựng phim cơ bản:</span> {element.slug}</div>
+            <div key={index} className="col-md-6 col-sm-12 col-lg-4 mt-3">
+                <div  className="course-item">
+                    <Link to={`details/${element.slug}`}> 
+                        <img className='course-image' src={element.image} alt="" />
+                    </Link>
+                    {
+                         like.length  === 0 ? 
+                        <div className=''
+                        onClick={() => handleLike(element.slug)}>
+                            <i class="fa-solid fa-heart"></i>
+                            <span>Thêm vào yêu thích</span>
+                        </div> :
+                        like.includes(element.slug) ? 
+                            <div className='like-wrapper active'
+                            onClick={() => handleLike(element.slug)}>
+                                <i class="fa-solid fa-heart" style={{color: "red"}}></i>  
+                                <span>Hủy thích</span>
+                            </div>
+                            :
+                            <div className='like-wrapper'
+                            onClick={() => handleLike(element.slug)}>
+                                 <i class="fa-solid fa-heart"></i>
+                                <span> Thêm vào yêu thích</span>
+                            </div>
+                    }
+                    <div className="course-body">
+                        <h3 className="course-name" onClick={() => {hanldeClick(element.slug)}}>{element.name}</h3>
+                        <div class="course-info"><span>Ngày khai giảng: </span>{element.startDate}</div>
+                        <div class="course-info"><span>Trình độ: </span>{element.level}</div>
+                        <div class="course-info"><span>Lịch học: </span>{element.schedule}</div>
+                        <div class="course-info"><span>Thời gian:</span> {element.time}</div>
+                        <div class="course-info"><span>Dựng phim cơ bản:</span> {element.slug}</div>
+                    </div>
                     <div className="course-bottom"> Giảm 15% học phí khi đăng ký trước 30/1/2022</div>
-                    <div class="MuiGrid-root mt-3 MuiGrid-item MuiGrid-grid-xs-12 MuiGrid-grid-md-4 MuiGrid-grid-lg-4"></div>
-                    <div class="wrap-course"></div> 
                 </div>
-                
-                
-                
             </div>
-            
         )
     })
     return(
-        // <div >
-        //     {/* <div className="row"> */}
-            <div className='container course-container'>
+        <div className='container course-container'>
             <div className="row course-list">
+                
                 {lists}
             </div>
         </div>
