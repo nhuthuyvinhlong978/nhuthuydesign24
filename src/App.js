ed3d2da08449ad0ae66ce5dacfd3f3a5177d44da
nhuthuyvinhlong978 <100194410+nhuthuyvinhlong978@users.noreply.github.com>
2022-02-24 16:47:26 +0700

    update fav function

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Fragment } from 'react';
+import { render } from 'react-dom';
 import Navigation from './Components/Navigation'
 import { BrowserRouter, Route, Routes, Switch } from 'react-router-dom';
 import './App.css'
